e75d44d84af19667b331fa16ea455841ad3fc185
Jeff <Jffrypwll@googlemail.com>
2018-04-09 21:56:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,3 +14,23 @@ then
   printf "\n\n Script must be run as root. \n\n"
   exit 1
 fi
+
+APACHE_INSTALLED=$(which apache2)
+if [[ "$APACHE_INSTALLED" == "" ]]
+then
+  printf "\n\n Installing Apache ...\n"
+  # Install Apache
+  apt-get install apache2 -y
+  update-rc.d apache2 enable
+  a2dissite 000-default.conf
+  service apache2 restart
+
+  APACHE_INSTALLED=$(which apache2)
+    if [[ "$APACHE_INSTALLED" == "" ]]
+    then
+      printf "\n\n EXITING : Apache installation FAILED\n"
+      exit 1
+    fi
+else
+  printf "\n\n Apache is already installed. \n"
+fi
