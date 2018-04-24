4105f9129c634f1801d1e6dbfa2d21a83b78a384
Jeff <Jffrypwll@googlemail.com>
2018-04-24 14:47:38 +0100

    Update mysqlsetup.sh

--- mysqlsetup.sh
+++ mysqlsetup.sh
@@ -19,18 +19,24 @@ SET PASSWORD FOR '$DB_USERNAME'@'$DB_SERVER' = PASSWORD('$DB_PASSWORD');
 GRANT ALL ON $DB_NAME.* TO '$DB_USERNAME'@'$DB_SERVER';
 FLUSH PRIVILEGES;
 USE $DB_NAME;
-CREATE TABLE IF NOT EXISTS systems   (      s_id          int(11)       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS system    (      id            int(11)       NOT NULL AUTO_INCREMENT PRIMARY KEY,
                                             name          varchar(256)  NOT NULL,
-                                            pin           int(11)       DEFAULT NULL,
-                                            active_level  tinyint(4)    DEFAULT NULL,
-                                            value         tinyint(1)    DEFAULT 0 );
-CREATE TABLE IF NOT EXISTS slevel   (      id            bigint(11)    NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                            ref           varchar(20)   DEFAULT NULL,
-                                            name          varchar(256)  DEFAULT NULL,
-                                            ip            varchar(16)   DEFAULT NULL,
-                                            value         float         DEFAULT NULL,
-                                            unit          varchar(11)   NOT NULL );
-
+                                            url           varchar(256)  NOT NULL,
+                                            port          tinyint(3)    DEFAULT NULL,
+                                            support       int(11)       DEFAULT 0 );
+CREATE TABLE IF NOT EXISTS metric    (      id            int(11)       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                            name          varchar(256)  NOT NULL,
+                                            query         varchar(1024) NOT NULL );
+CREATE TABLE IF NOT EXISTS channel   (      id            int(11)       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                            name          varchar(256)  NOT NULL,
+                                            action        varchar(1024) NOT NULL );
+CREATE TABLE IF NOT EXISTS alert     (      id            int(11)       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                            system_id     int(11)       NOT NULL,
+                                            metric_id     int(11)       NOT NULL,
+                                            trigger_value float         NOT NULL,
+                                            trigger_opp   char(1)       NOT NULL,
+                                            channel_id    int(11)       NOT NULL,
+                                            status        tinyint(1)    DEFAULT 0 );
 DATABASE
 
 cat > /opt/proadmin/proadmin-master/config.ini <<CONFIG
